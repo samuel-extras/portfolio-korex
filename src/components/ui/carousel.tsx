95a934822ad50af0ec9d6b3386f6a6e432509436
Samuel Ajibola <samuel88783.so@gmail.com>
2024-01-05 21:34:38 +0100

    refactoring

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,9 +1,9 @@
 "use client";
 import * as React from "react";
-import useEmblaCarousel from // type EmblaOptionsType as CarouselOptions, // type EmblaCarouselType as CarouselApi,
-// type EmblaPluginType as CarouselPlugin,UseEmblaCarouselType
+import useEmblaCarousel from // type EmblaPluginType as CarouselPlugin,UseEmblaCarouselType // type EmblaOptionsType as CarouselOptions, // type EmblaCarouselType as CarouselApi,
 "embla-carousel-react";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+// resolve this
+// import { ArrowLeft, ArrowRight } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
